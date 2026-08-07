c79795df157c7c8b35ffd8c9b01156426709667c
Dark Factory <factory@darkfactory.allcode.com>
2026-08-07 04:12:59 +0000

    Add getBenchmarkValue function in benchmarkSimple.ts

---
+++ src/queries/benchmarkSimple.ts
@@ -0,0 +1,11 @@
+interface BenchmarkValue {
+  value: number;
+  message: string;
+}
+
+export function getBenchmarkValue(): BenchmarkValue {
+  return {
+    value: 42,
+    message: "benchmark",
+  };
+}
