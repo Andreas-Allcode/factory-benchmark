9a2728ea290dbd8e2d2629f887df761072f17943
Dark Factory <factory@nexusfactory.allcode.com>
2026-08-12 22:27:30 +0000

    Add BenchmarkCard React component

---
+++ src/components/BenchmarkCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export interface BenchmarkCardProps {
+  title: string;
+  value: number;
+}
+
+const BenchmarkCard: React.FC<BenchmarkCardProps> = ({ title, value }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{value}</p>
+    </div>
+  );
+};
+
+export default BenchmarkCard;
